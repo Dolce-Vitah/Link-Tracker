a80682a74e3bb4b2850d7d52ff92715267cb542a
Dolce-Vitah <vadenisenko@edu.hse.ru>
2026-02-22 23:00:49 +0300

    feat: add start command handler

---
+++ internal/application/commands/start.go
@@ -0,0 +1,19 @@
+package commands
+
+import (
+	"log/slog"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+type StartCommand struct {}
+
+func (c *StartCommand) Name() string { return "start" }
+func (c *StartCommand) Description() string { return "Начать работу с ботом" }
+
+func (c *StartCommand) Handle(update *tgbotapi.Update, bot Sender) error {
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Добро пожаловать! Используйте /help, чтобы узнать о доступных командах.")
+	_, err := bot.Send(msg)
+
+	slog.Info("Start command processed", slog.Int64("chat_id", update.Message.Chat.ID))
+	return err
+}
