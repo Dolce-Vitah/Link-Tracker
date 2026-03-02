81a051e3b63e58ca716f8b8f627d0d7e086e3ebe
Dolce-Vitah <vadenisenko@edu.hse.ru>
2026-03-02 16:15:58 +0300

    refactor: move dependency initialization and bot start to App struct

---
+++ cmd/bot/app.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log/slog"
+
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/application/commands"
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/infrastructure/config"
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/infrastructure/telegram"
+)
+
+type App struct {
+	bot *telegram.Bot
+}
+
+func (a *App) New() error {
+	cfg, err := config.Load("config.json")
+	if err != nil {
+		return err
+	}
+
+	bot, err := telegram.NewBot(cfg.TelegramToken)
+	if err != nil {
+		return err
+	}
+
+	bot.RegisterCommand(&commands.StartCommand{})
+	bot.RegisterCommand(&commands.HelpCommand{})
+
+	if err := bot.SetMyCommands(); err != nil {
+		slog.Error("Failed to set bot commands menu", slog.String("error", err.Error()))
+	} else {
+		slog.Info("Bot commands menu updated successfully in UI")
+	}
+
+	a.bot = bot
+	return nil
+}
+
+func (a *App) Run() {
+	slog.Info("Bot is up and running...")
+	a.bot.Start()
+}
--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -3,37 +3,17 @@ package main
 import (
 	"log/slog"
 	"os"
-
-	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/application/commands"
-	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/infrastructure/config"
-	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/infrastructure/telegram"
 )
 
 func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	slog.SetDefault(logger)
 
-	cfg, err := config.Load("config.json")
-	if err != nil {
-		slog.Error("Failed to load config", slog.String("error", err.Error()))
+	var app App
+	if err := app.New(); err != nil {
+		slog.Error("Failed to initialize app", slog.String("error", err.Error()))
 		os.Exit(1)
 	}
 
-	bot, err := telegram.NewBot(cfg.TelegramToken)
-	if err != nil {
-		slog.Error("Failed to create bot", slog.String("error", err.Error()))
-		os.Exit(1)
-	}
-
-	bot.RegisterCommand(&commands.StartCommand{})
-	bot.RegisterCommand(&commands.HelpCommand{})
-
-	if err := bot.SetMyCommands(); err != nil {
-		slog.Error("Failed to set bot commands menu", slog.String("error", err.Error()))
-	} else {
-		slog.Info("Bot commands menu updated successfully in UI")
-	}
-
-	slog.Info("Bot is up and running...")
-	bot.Start()
+	app.Run()
 }
