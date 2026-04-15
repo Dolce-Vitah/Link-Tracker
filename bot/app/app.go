7c80ecefcccc0a00db30db69e63d80e55f265f8e
Dolce-Vitah <vadenisenko@edu.hse.ru>
2026-04-15 16:52:27 +0300

    style: remove empty lines

--- bot/app/app.go
+++ bot/app/app.go
@@ -48,21 +48,13 @@ func (a *App) New() error {
 	}
 
 	sessions := bot.Sessions()
-
 	chatHandler := chat.NewChatHandler(trackerService, bot.Client(), slog.Default())
-
 	linkHandler := link.NewLinkHandler(sessions, trackerService, bot.Client(), slog.Default())
-
 	bot.RegisterCommand(chat.NewStartCommand(chatHandler))
-
 	bot.RegisterCommand(chat.NewHelpCommand(chatHandler))
-
 	bot.RegisterCommand(link.NewTrackCommand(linkHandler))
-
 	bot.RegisterCommand(link.NewUntrackCommand(linkHandler))
-
 	bot.RegisterCommand(link.NewListCommand(linkHandler))
-
 	bot.RegisterCommand(link.NewCancelCommand(linkHandler))
 
 	setCommandsErr := bot.SetMyCommands()
